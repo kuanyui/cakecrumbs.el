002bf48cd7be5bb9aaa09219e7b2aeb2b6380618
kuanyui <azazabc123@gmail.com>
2017-10-27 02:58:04 +0800

    init!

---
+++ levelcrumbs.el
@@ -0,0 +1,120 @@
+;;; levelcrumbs.el ---                               -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  onohiroko
+
+;; Author: onohiroko <onohiroko@kuanyenMBP>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(which-function-mode)
+(setq which-func-format
+      '( "" (:propertize which-func-current local-map
+                         (keymap (mode-line keymap (mouse-3 . end-of-defun)
+                                            (mouse-2 . #[nil "" [1 narrow-to-defun] 2 nil nil])
+                                            (mouse-1 . beginning-of-defun)
+                                            ))
+                         face which-func mouse-face mode-line-highlight help-echo
+                         "mouse-1: go to beginning
+mouse-2: toggle rest visibility
+mouse-3: go to end") ""))
+
+(delete (assoc 'which-func-mode mode-line-misc-info) mode-line-misc-info)
+(setq which-func-header-line-format '(which-func-mode ("" which-func-format)))
+
+(defun which-func-setup-header ()
+  (interactive)
+  (when which-func-mode
+    (delete (assoc 'which-func-mode mode-line-misc-info) mode-line-misc-info)
+    (setq header-line-format which-func-header-line-format)))
+
+(setq which-func-functions '(which-func-scss-fn))
+
+(add-hook 'prog-mode-hook 'which-func-setup-header t t)
+(add-hook 'html-mode-hook 'which-func-setup-header t t)
+
+(defun which-func-scss-fn ()
+  (when (eq major-mode 'omg-scss-mode)
+
+    (save-excursion
+      (let* ((parent-pos-list (nth 9 (syntax-ppss)))
+             (parent-list (mapcar (lambda (pos)
+                                    (goto-char pos)
+                                    (let* ((to (point))
+                                           (from (progn (re-search-backward "[,;{}^\n]" nil t)
+                                                        (1+ (point)))))
+                                      (string-trim (buffer-substring from to)))) parent-pos-list)))
+        (string-join parent-list (propertize "  " 'face 'font-lock-comment-face))
+        ))))
+  ;; (format "point == %s , %s" (point) (parse-partial-sexp (point-min) (point-max) nil nil (syntax-ppss)))
+(save-excursion (syntax-after (point))
+                (skip-syntax-forward "^()" 3)
+                (syntax-ppss))
+
+(defun levelcrumbs-string-match (regexp num string)
+  (save-match-data
+    (if (string-match regexp string)
+        (match-string num string)
+      nil)))
+
+(defun levelcrumbs-html-parent-position (&optional point)
+  (interactive)
+  (save-excursion
+    (save-match-data
+      (let (back-until-tag-name tag-name tag-pos)
+        (while (let* ((tag-pos (re-search-backward "< *\\(\\(?:.\\|\n\\)*?\\) *>" 0 t))
+                      (raw-tag (and tag-pos (match-string-no-properties 1)))
+                      (slash (cond ((string-prefix-p "/" raw-tag) 'left)
+                                   ((string-suffix-p "/" raw-tag) 'right)
+                                   (t nil))))
+                 (if tag-pos
+                     (setq tag-name (cond ((eq slash 'left)  (levelcrumbs-string-match "^/ *\\([^>< ]+\\)" 1 raw-tag))
+                                          ((eq slash 'right) (levelcrumbs-string-match "[^>< ]+ *$" 0 raw-tag))
+                                          (t                 (levelcrumbs-string-match "^[^>< ]+" 0 raw-tag)))))
+                 (cond ((null tag-pos) nil) ; nil: terminate while loop
+                       (back-until-tag-name
+                        (if (string-equal back-until-tag-name tag-name) ; if equal, set to nil && continue. else, continue.
+                            (progn (setq back-until-tag-name nil) t))
+                        t) ; always continue while loop
+                       ((string-match "^\\(link\\|a\\|img\\)\\b" raw-tag) t) ; self-closing tag (<img/> or <img>)
+                       ((and slash (eq slash 'right)) t) ; self-closing tag (<hr/>)
+                       ((and slash (eq slash 'left) raw-tag) ; closing-tag (</title>)
+                        (setq back-until-tag-name tag-name)
+                        t)
+                       (t nil)  ;; found parent!
+                       ))) ; WHILE
+        (message (format "%s -- %s" (point) tag-name)))
+      )))
+
+(defun ttt ()
+  (interactive)
+  (save-excursion
+    (message "%s" (parse-partial-sexp (point-min) (point-max) nil nil (syntax-ppss)))))
+
+(defun sss ()
+  (interactive)
+  (message (format "POS ==> %s, CONTEXT ==> %s" (point) (save-excursion (sgml-get-context)))))
+
+;; In nxml-mode,  `nxml-backward-up-element' to go up to parent.
+;; In web-mode, C-c C-e u parent element (up) (`web-mode-element-parent-position' (point))
+
+
+(provide 'levelcrumbs)
+;;; levelcrumbs.el ends here
